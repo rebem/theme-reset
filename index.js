69498ea177d934499f95779778ba563278506553
deepsweet <kir@soulshine.in>
2016-01-05 10:23:27 +0700

    :wrench: bump with new layers-loader configs format

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 var path = require('path');
 
 module.exports = {
-    default: {
-        path: path.resolve(__dirname, 'build/'),
-        files: {
-            styles: 'styles.css'
-        }
-    },
-    src: {
-        path: path.resolve(__dirname, 'src/'),
-        files: {
-            styles: 'styles.less'
-        }
+    path: path.resolve(__dirname, 'build/'),
+    files: {
+        styles: 'styles.css'
     }
 };
